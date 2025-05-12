1743c9be63a6c0bdc7d7affdc0932aa6ae5ac50a
Mohd Jaid <86649203+mohdjaid@users.noreply.github.com>
2025-05-13 01:31:51 +0530

    Commit #1

---
+++ flightApp/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from flightApp.models import Flight, Passenger, Reservation
+import re
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = '__all__'
+
+    def validate_flightNumber(self, flightNumber):
+        if(re.match("^[a-zA-Z0-9-]*$", flightNumber) == None):
+            raise serializers.ValidationError("Invalid number. Make sure it is Alpha numeric")
+        return flightNumber
+
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passenger
+        fields = '__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
